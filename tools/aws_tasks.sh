293205e8fbad5ee7bdf9d5d46aa57abafe02dccc
Kjell Morgenstern <kjell@gmx.de>
2019-06-17 14:20:43 +0200

    shellcheck

--- tools/aws_tasks.sh
+++ tools/aws_tasks.sh
@@ -10,22 +10,22 @@ function setup {
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-    EOL
+EOL
 
     # set up awscli packages
     pip install --user awscli
-    mkdir -p ~/$TRAVIS_BUILD_NUMBER
-    aws s3 sync s3://fritzing/$TRAVIS_BUILD_NUMBER ~/$TRAVIS_BUILD_NUMBER
+    mkdir -p ~/"$TRAVIS_BUILD_NUMBER"
+    aws s3 sync s3://fritzing/"$TRAVIS_BUILD_NUMBER" ~/"$TRAVIS_BUILD_NUMBER"
 }
 
 function synchronize {
-    aws s3 sync ~/$TRAVIS_BUILD_NUMBER s3://fritzing/$TRAVIS_BUILD_NUMBER;
+    aws s3 sync ~/"$TRAVIS_BUILD_NUMBER" s3://fritzing/"$TRAVIS_BUILD_NUMBER";
 }
 
 function cleanup {
-    aws s3 rm --recursive s3://fritzing/$TRAVIS_BUILD_NUMBER # clean up after ourselves
+    aws s3 rm --recursive s3://fritzing/"$TRAVIS_BUILD_NUMBER" # clean up after ourselves
 }
 
-if [[ ( $TRAVIS_PULL_REQUEST == false ) && ( $TRAVIS_BRANCH = "develop" ) ]]; then 
+if [[ ( "$TRAVIS_PULL_REQUEST" == false ) && ( "$TRAVIS_BRANCH" = "develop" ) ]]; then
     $1;
 fi
